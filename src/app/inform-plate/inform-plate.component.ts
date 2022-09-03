2aed8ffd7d45b6966f08b6c7fee05a8774b86894
Gabriel Simon <gabrielsimon775@gmail.com>
2022-09-02 23:58:46 -0300

    add style in html and scss

--- src/app/inform-plate/inform-plate.component.ts
+++ src/app/inform-plate/inform-plate.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
-import { InformPlate } from "./interfaces/inform-plate";
 import { InformPlateService } from "./services/inform-plate.service";
 
 @Component({
@@ -11,26 +10,16 @@ import { InformPlateService } from "./services/inform-plate.service";
 export class InformPlateComponent implements OnInit {
   formGroup!: FormGroup;
 
-  constructor(
-    private informPlateService: InformPlateService,
-    private formBuilder: FormBuilder,
-    ) {}
+  constructor(private informPlateService: InformPlateService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       plate: [undefined]
-    })
+    });
   }
 
   public insert() {
-    const plate = this.formGroup.get('plate')?.value as InformPlate;
-    return this.informPlateService.inserirPlaca(plate).subscribe();
-  }
-
-  public validateInput() {
     const plate = this.formGroup.get('plate')?.value;
-    if(plate == undefined || plate == "" || plate == null) {
-
-    }
+    return this.informPlateService.inserirPlaca(plate).subscribe();
   }
 }
